37b71ae16ec4d952dc6dad6b6ed1640668478fab
hafizhsparta <102685316+hafizhsparta@users.noreply.github.com>
2022-04-07 06:48:37 +0700

    Update 008_download_and_convert_b23_subtitle.py

--- Python/008_download_and_convert_b23_subtitle.py
+++ Python/008_download_and_convert_b23_subtitle.py
@@ -20,7 +20,7 @@ def obtain_cc_subtitle(vid: str = "") -> None:
 
     $vid: id of the video
     """
-    url = f"https://www.bilibili.com/video/{vid}"
+    url = f"https://www.bilibili.tv/en/play/1049994/11259573{vid}"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 "
         "(KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
